9e244c208487ffe35da73e5524c9ad0cf24bc324
jeolje934-dot <jeolje934@gmail.com>
2026-06-03 02:42:36 +0900

    260603 Room1~4 플레이어 이동 , 포탈 장치 이동 가능 ( 포탈 , 플레이어 임시 블록 사용 )

---
+++ 엔진 기말 (수)/Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 7.0f;  //이동 속도
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    
+    void Update()
+    {
+        //입력 감지
+        movement.x = 0;
+        movement.y = 0;
+
+
+        if (Input.GetKey(KeyCode.W)) movement.y = 1;      // 위
+        if (Input.GetKey(KeyCode.S)) movement.y = -1;     // 아래
+        if (Input.GetKey(KeyCode.A)) movement.x = -1;     // 왼쪽
+        if (Input.GetKey(KeyCode.D)) movement.x = 1;      // 오른쪽 
+
+        //  대각선 이동시 속도가 빨라지는 것 방지 (정규화)
+        movement = movement.normalized;
+ 
+    }
+
+    private void FixedUpdate()
+    {
+        //물리 엔진을 통한 이동
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ 엔진 기말 (수)/Assets/Script/Portal.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Portal : MonoBehaviour
+{
+
+    [Header("이동 설정")]
+    public string targetSceneName;   // 이동할 씬 이름
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 닿은 대상의 태그가 "Player"인지 확인
+        if (collision.CompareTag("Player"))
+        {
+            // 씬 이름이 비어있지 않다면 이동을 실행
+            if (!string.IsNullOrEmpty(targetSceneName))
+            {
+                SceneManager.LoadScene(targetSceneName);
+            }
+            else
+            {
+                Debug.LogWarning("이동할 씬 이름이 설정되지 않았습니다.");
+            }
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
