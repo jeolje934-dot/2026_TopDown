7121702a426277a16e9fce9d5672243a2a6b6cc4
jeolje934-dot <jeolje934@gmail.com>
2026-06-22 16:04:01 +0900

    [0622] BoxData(ScriptableObject) , BoxSpawner 스크립트 작성 , Json 데이터 저장 클래스 작성 . Box 이미지 생성

---
+++ 엔진 기말 (수)/Assets/Script/BoxData.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBoxData", menuName = "Data/BoxData")]
+
+[System.Serializable]
+public class BoxSaveData
+{
+    public float x, y; // 상자 위치
+}
+
+[System.Serializable]
+public class RoomSaveData
+{
+    public List<BoxSaveData> boxes = new List<BoxSaveData>();
+}
+
+public class BoxData : ScriptableObject
+{
+    public GameObject boxPrefab; // 상자 프리팹
+    public int spawnCount = 15;  // 방마다 생성할 상자 개수
+}
---
+++ 엔진 기말 (수)/Assets/Script/BoxSpawner.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.IO;
+using System.Collections.Generic;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public BoxData boxData;
+    public Tilemap groundTilemap;
+
+    void Start()
+    {
+        string filePath = Application.persistentDataPath + "/" + gameObject.scene.name + "_" + gameObject.name + ".json";
+
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            RoomSaveData data = JsonUtility.FromJson<RoomSaveData>(json);
+            foreach (var b in data.boxes)
+            {
+                Instantiate(boxData.boxPrefab, new Vector3(b.x, b.y, 0), Quaternion.identity, this.transform);
+            }
+        }
+        else
+        {
+            SpawnAndSave(filePath);
+        }
+    }
+
+    // 타일맵에서 랜덤한 위치 리스트를 가져오는 핵심 함수
+    List<Vector3> GetRandomPositions()
+    {
+        List<Vector3> allPositions = new List<Vector3>();
+        BoundsInt bounds = groundTilemap.cellBounds;
+
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int localPlace = new Vector3Int(x, y, 0);
+                if (groundTilemap.HasTile(localPlace))
+                {
+                    allPositions.Add(groundTilemap.CellToWorld(localPlace) + new Vector3(0.5f, 0.5f, 0));
+                }
+            }
+        }
+
+        // 셔플
+        for (int i = 0; i < allPositions.Count; i++)
+        {
+            int randomIndex = Random.Range(i, allPositions.Count);
+            Vector3 temp = allPositions[i];
+            allPositions[i] = allPositions[randomIndex];
+            allPositions[randomIndex] = temp;
+        }
+
+        // 15개만 리턴
+        int count = Mathf.Min(boxData.spawnCount, allPositions.Count);
+        return allPositions.GetRange(0, count);
+    }
+
+    void SpawnAndSave(string path)
+    {
+        List<Vector3> positions = GetRandomPositions();
+        RoomSaveData data = new RoomSaveData();
+
+        foreach (var pos in positions)
+        {
+            Instantiate(boxData.boxPrefab, pos, Quaternion.identity, this.transform);
+            data.boxes.Add(new BoxSaveData { x = pos.x, y = pos.y });
+        }
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(path, json);
+    }
+}
